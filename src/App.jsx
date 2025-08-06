ef758e98b84769afb127334265bac77d25caf32a
Kshitij <kshitijkumar514@gmail.com>
2025-08-06 21:38:15 +0530

    Add 404 NotFound page and route handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Results from './components/Results';
 import PaymentForm from './components/checkout/PaymentForm';
 import PaymentSuccess from './components/checkout/PaymentSuccess';
 import Navbar from './components/shared/Navbar';
+import NotFound from './components/NotFound';
 
 
 
@@ -18,6 +19,7 @@ function App(){
                     <Route path='/results' element={<Results/>}/>
                     <Route path='/payment' element={<PaymentForm/>}/>
                     <Route path='/payment-success' element={<PaymentSuccess/>}/>
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </Router>
         </React.Fragment>
